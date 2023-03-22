c131f8601a82764b8c390dc3c8b1cba148227e02
yjin0034 <yjin0034@naver.com>
2023-03-22 15:15:45 +0900

    01. Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className='App'>
+      <header>
+        <a href='/'>Mancity Goods Shop</a>
+      </header>
+      <main>
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;

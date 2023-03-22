940929424adb8ef39c9f5ce32526d4922130d1e7
yjin0034 <yjin0034@naver.com>
2023-03-22 19:18:35 +0900

    2. 상품 리스트 페이지 구현

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import data from "./data";
 
 function App() {
   return (
@@ -6,7 +7,25 @@ function App() {
         <a href='/'>Mancity Goods Shop</a>
       </header>
       <main>
-        list products
+        <h1>추천 상품</h1>
+        <div className='products'>
+          {data.products.map((product) => (
+            <div className='product' key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className='product-info'>
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>{product.price}원</strong>
+                </p>
+                <button>장바구니 담기</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
   );

89f42e9fce946f67bbf7f5ddefd95ff7d76b07e4
yjin0034 <yjin0034@naver.com>
2023-03-23 17:19:09 +0900

    03. Routing 추가

--- README.md
+++ README.md
@@ -4,4 +4,7 @@
 
 1. 초기 리액트 설치 및 프론트엔드 환경 구성
 2. 상품 리스트 페이지 구현
-   4가지 예시 상품 추가
+   - 4가지 예시 상품 추가
+3. Routing 추가
+   - react-router-dom 활용
+   - 홈(메인) 페이지(Homescreen), 제품 페이지(ProductScreen) 경로 추가
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+import data from "../data";
+
+function HomeScreen() {
+  return (
+    <div>
+      <h1>추천 상품</h1>
+      <div className='products'>
+        {data.products.map((product) => (
+          <div className='product' key={product.slug}>
+            <Link to={`/product/${product.slug}`}>
+              <img src={product.image} alt={product.name} />
+            </Link>
+            <div className='product-info'>
+              <Link to={`/product/${product.slug}`}>
+                <p>{product.name}</p>
+              </Link>
+              <p>
+                <strong>{product.price}원</strong>
+              </p>
+              <button>장바구니 담기</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+export default HomeScreen;
